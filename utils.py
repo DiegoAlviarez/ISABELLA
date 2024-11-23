c58414c7fe94668e93436e7463aa0de478a3e584
DiegoAlviarez <dilanalviarez@gmail.com>
2024-11-23 00:49:27 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
-# utils.py
 import pandas as pd
 import requests
+from PIL import Image
 from io import BytesIO
+
 
 def load_data(url):
     """
@@ -10,13 +11,39 @@ def load_data(url):
     :return: pd.DataFrame con los datos o None si falla.
     """
     try:
+        # Descarga el contenido desde la URL
         response = requests.get(url, timeout=10)
-        response.raise_for_status()  # Lanza error si el código HTTP no es 200
+        response.raise_for_status()  # Lanza un error si el código HTTP no es 200
+        
+        # Carga el archivo Excel en un DataFrame
         data = pd.read_excel(BytesIO(response.content))
         return data
     except requests.exceptions.RequestException as e:
-        print(f"Error al cargar datos: {e}")
+        print(f"Error al descargar el archivo: {e}")
         return None
     except Exception as e:
-        print(f"Error procesando el archivo: {e}")
+        print(f"Error al procesar el archivo Excel: {e}")
         return None
+
+
+def load_image(url):
+    """
+    Carga una imagen desde una URL.
+    :param url: str, URL de la imagen.
+    :return: PIL.Image o None si falla.
+    """
+    try:
+        # Descarga el contenido desde la URL
+        response = requests.get(url, timeout=10)
+        response.raise_for_status()  # Lanza un error si el código HTTP no es 200
+        
+        # Carga la imagen en un objeto PIL.Image
+        image = Image.open(BytesIO(response.content))
+        return image
+    except requests.exceptions.RequestException as e:
+        print(f"Error al descargar la imagen: {e}")
+        return None
+    except Exception as e:
+        print(f"Error al procesar la imagen: {e}")
+        return None
+
